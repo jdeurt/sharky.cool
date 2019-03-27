d521669b36b53a9793233de4fbc467cf69a105c4
Juan de Urtubey <jdeurt@gmail.com>
2019-03-26 22:39:08 -0500

    Added redirect option.

--- controllers/api/ifunny/rip.js
+++ controllers/api/ifunny/rip.js
@@ -7,18 +7,15 @@ module.exports = (req, res) => {
         return res.status(400).send("Invalid url param.");
     }
 
-    console.log("Matched pattern.");
-
     got.get(req.query.url).then(data => {
         const $ = cheerio.load(data.body);
 
-        console.log("Loaded into cheerio.");
-
         let wrapper = $(".media__wrapper .media__content script").html();
         let poster = wrapper.match(/poster=".+?"/)[0].replace(/_3\.(png|jpg|jpeg)/, "_1.mp4").replace("images", "videos");
-        console.log(poster);
+        let src = poster.match(/".+?"/)[0].replace(/"/g, "");
 
-        res.send(poster.match(/".+?"/)[0].replace(/"/g, ""));
+        if (req.query.redirect) res.redirect(src);
+        res.send(src);
     }).catch(err => {
         console.log(err);
 
