e0b649fd30982239b6fd4978f241ad3867c0ba66
Juan de Urtubey <jdeurt@gmail.com>
2019-04-02 14:49:58 -0500

    Added nice animation thing.

---
+++ stuff/circles/circles.js
@@ -0,0 +1,58 @@
+const app = new PIXI.Application({
+    autoResize: true,
+    resolution: devicePixelRatio,
+    antialias: true,
+    transparent: true
+});
+document.body.appendChild(app.view);
+
+let data = {
+    objects: []
+};
+
+window.setInterval(function() {
+    let color = parseInt(randomColor().replace("#", "0x"));
+
+    let circle = new PIXI.Sprite(PIXI.Texture.WHITE);
+        circle.tint = color;
+        circle.width = 0;
+        circle.height = 0;
+        circle.anchor.set(0.5);
+        circle.x = app.screen.width / 2;
+        circle.y = app.screen.height / 2;
+
+    app.stage.addChild(circle);
+
+    let count = 0;
+
+    data.objects.push({
+        circle,
+        count,
+        color
+    });
+}, 500);
+
+app.ticker.add(function(delta) {
+    data.objects.forEach((object, index) => {
+        if (object.circle.width > app.screen.width + 100 && object.circle.height > app.screen.height + 100) {
+            document.getElementById("root").style = `background-color: #${object.color.toString(16)}`;
+            app.stage.removeChild(object.circle);
+            data.objects.splice(index, 1);
+        }
+
+        object.count += 0.1;
+        let count = object.count;
+
+        object.circle.width = count * 100;
+        object.circle.height = count * 100;
+        object.circle.rotation = count * 0.1;
+    });
+});
+
+window.addEventListener('resize', resize);
+
+function resize() {
+	app.renderer.resize(window.innerWidth, window.innerHeight);
+}
+
+resize();
