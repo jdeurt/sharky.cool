23fc18c1a07ac33c3d2ebfe300f5e793054e6c4c
Juan de Urtubey <jdeurt@gmail.com>
2019-05-20 13:48:33 -0500

    Fixed typo.

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 // Served by reverse proxy :P.
-server.listen("4202", () => {
-    console.log("Listening on port 4202");
-});
+server.listen("4202");
+console.log("Listening on port 4202");
 
 module.exports = {
     app,
